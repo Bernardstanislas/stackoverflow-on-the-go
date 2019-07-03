0a572821f3277505912d2311a12715719a271633
Stanislas Bernard <bernardstanislas@gmail.com>
2019-07-03 22:47:00 +0200

    ES indexation asks for the min and max id

--- routes/console.php
+++ routes/console.php
@@ -21,7 +21,31 @@ Artisan::command('inspire', function () {
 
 Artisan::command('posts:index', function () {
     $this->comment('Starting posts indexation in Elastic Search');
-    $this->info(Post::count() . ' posts to index');
-    Post::addAllToIndex();
+    $userDefinedMinPostId = $this->ask('What id to you want to start indexing from?');
+    $userDefinedMaxPostId = $this->ask('What id to you want to start indexing to?');
+    $totalPostCount = 17738709;
+    $minPostId = 4;
+    $maxPostId = 56412356;
+
+    $approximatePostCount = intval(($userDefinedMaxPostId - $userDefinedMinPostId) * $totalPostCount / ($maxPostId - $minPostId));
+    $this->info('Approximately ' . $approximatePostCount . ' posts to index');
+    $approximatePostCount = 17738709;
+    $chunkNumber = 0;
+    $chunkSize = 2000;
+    Post::orderBy('id', 'ASC')
+        ->where([
+            ['id', '>=', $userDefinedMinPostId],
+            ['id', '<', $userDefinedMaxPostId]
+        ])
+        ->chunk($chunkSize, function ($posts) use (&$chunkNumber, &$approximatePostCount, &$chunkSize) {
+            $this->info(round($chunkNumber * $chunkSize * 100 / $approximatePostCount, 1) . '% done');
+            $this->info('Memory used ' . memory_get_usage());
+            $timeStarted = microtime(true);
+            $posts->addToIndex();
+            $this->info('Done with chunk ' . $chunkNumber . ', took ' . (microtime(true) - $timeStarted) . 'ms');
+            gc_collect_cycles();
+            $chunkNumber++;
+        });
+
     $this->comment('Done indexing posts');
 })->describe('Index Posts from database in the Elastic Search index');
